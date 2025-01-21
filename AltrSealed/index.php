f71d98576b7f52b7d81bd0104482c6b31d06cd7e
Tomlave <99726636+tomlave@users.noreply.github.com>
2025-01-22 00:25:50 +0100

    Add files via upload

---
+++ AltrSealed/index.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(1);
+ini_set("display_errors", 1);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<?php
+include 'serveur/head.php';
+?>
+</head>
+
+<body>
+<?php
+include 'serveur/home.php'
+?>
+</body>
+
+</html>
---
+++ AltrSealed/serveur/head.php
@@ -0,0 +1,3 @@
+<title>Altered Sealed</title>
+<link rel="icon" type="image/x-icon" href="isek.ico">
+<link rel="stylesheet" href="styles.css">
