e404c456730b1f81217abaf912d3db18a436c026
Tomlave <99726636+tomlave@users.noreply.github.com>
2025-07-26 18:43:25 +0200

    Add files via upload

--- AlteredAPI.py
+++ AlteredAPI.py
@@ -1,17 +1,10 @@
-#Import les dependance
+# Import les dependance
 import requests
 import os
 import json
 
-#Import le variable
-from config import (
-  API_URL,
-  SETALL,
-  ItemPerPage,
-  PAGE,
-  headers,
-  DOWNLOAD_PNG
-)
+# Import le variable
+from config import (API_URL, SETALL, ItemPerPage, PAGE, headers, DOWNLOAD_PNG)
 
 for SET in SETALL:
 	SETCODE = SET[1]
@@ -23,7 +16,7 @@ for SET in SETALL:
 	SAVE_PATH_JSON = './AlteredSealed/asset/'+SETNAME
 	try:
 		if DOWNLOAD_PNG:
-			#Crée le chemin d'export
+			# Crée le chemin d'export
 			os.makedirs(SAVE_PATH_IMG)
 			os.makedirs(SAVE_PATH_JSON)
 	except :
@@ -33,12 +26,12 @@ for SET in SETALL:
 	JSON_CARDS_COMMON = []
 	JSON_CARDS_RARE = []
 
-	#Debut du script - boucle sur le nombre de PAGE
-	for runPage in range(1,PAGE):
-		#Appel l'API
-		response = requests.get(API_URL+'?cardSet[]='+SETCODE+'&ItemPerPage='+str(ItemPerPage)+'&page='+str(runPage), headers=headers)
+	# Debut du script - boucle sur le nombre de PAGE
+	for runPage in range(1, PAGE):
+		# Appel l'API
+		response = requests.get(API_URL+'?cardSet[]='+SETCODE+'&ItemPerPage='+str(ItemPerPage)+'&page='+str(runPage)+'&locale= ', headers=headers)
 
-		#Verifies si la connection à reussi
+		# Verifies si la connection à reussi
 		if response.status_code == 200:
 
 			# place la reponce de l'API dans un json
@@ -58,7 +51,7 @@ for SET in SETALL:
 				if (TypeCarte != 'Jeton Personnage' and TypeCarte != 'Foiler' and TypeCarte != 'Mana'):
 					# Place tout les info de la carte dans des variable
 					# Numero de la carte
-					NameJPG = (jsonFile['hydra:member'][runCarte]['collectorNumberFormatted'])
+					NameJPG = (jsonFile['hydra:member'][runCarte]['id'])
 					# Code de reference
 					ReferenceJPG = (jsonFile['hydra:member'][runCarte]['reference'])
 					# Rarter de la carte (Commune,Rare,Out Of Faction)
@@ -90,45 +83,41 @@ for SET in SETALL:
 						DataForest = 0
 						DataMountain = 0
 						DataOcean = 0
-
-
 					# Lien de image png
 					LinkJPG = (jsonFile['hydra:member'][runCarte]['imagePath'])
 					# Requet le lien de l'image
-					responseJPG = requests.get(LinkJPG,allow_redirects=True)
+					responseJPG = requests.get(LinkJPG, allow_redirects=True)
 
 					# Si carte hero ajouter à la variable d'export json pour hero
-					if TypeCarte == 'Héros':
-						JSON_HEROS.append([NameJPG,DataFaction,DataName,DataMain,DataRecall,ReferenceJPG,DataForest,DataMountain,DataOcean,TypeCarte,LinkJPG])
+					if TypeCarte == 'Hero':
+						JSON_HEROS.append([NameJPG, DataFaction, DataName, DataMain, DataRecall, ReferenceJPG, DataForest, DataMountain, DataOcean, TypeCarte, LinkJPG])
 
 					# Si carte n'est pas hero ajouter à la variable d'export json pour les non hero
 					else:
 						# Si carte commune ajouter à la variable d'export json pour commune
 						if RarityJPG == 'COMMON':
-							print(NameJPG)
-							JSON_CARDS_COMMON.append([NameJPG,DataFaction,DataName,DataMain,DataRecall,ReferenceJPG,DataForest,DataMountain,DataOcean,TypeCarte,LinkJPG])
+							JSON_CARDS_COMMON.append([NameJPG, DataFaction, DataName, DataMain, DataRecall, ReferenceJPG, DataForest, DataMountain, DataOcean, TypeCarte, LinkJPG])
 						# Si carte non commune ajouter à la variable d'export json pour carte rare et oof
 						else:
-							print(NameJPG)
-							JSON_CARDS_RARE.append([NameJPG,DataFaction,DataName,DataMain,DataRecall,ReferenceJPG,DataForest,DataMountain,DataOcean,TypeCarte,LinkJPG])
+							JSON_CARDS_RARE.append([NameJPG, DataFaction, DataName, DataMain, DataRecall, ReferenceJPG, DataForest, DataMountain, DataOcean, TypeCarte, LinkJPG])
 
-					if DOWNLOAD_PNG:
 					# Si lien image fonctionne télècharge les image
+					if DOWNLOAD_PNG:
 						if responseJPG.status_code == 200:
 							with open(SAVE_PATH_IMG+NameJPG+".png", 'wb') as f:
 								f.write(responseJPG.content)
 
-				#ici fin de la boucle carte pass a la suivante de la page
-		#ici fin de la boucle page pass a la suivante ou fin de script
+				# ici fin de la boucle carte pass a la suivante de la page
+		# ici fin de la boucle page pass a la suivante ou fin de script
 
 	# Crée les 3 json
 	with open(SAVE_PATH_JSON+"heros.json", 'w') as h:
 		# Formatage et mise dans le json
-		json.dump(JSON_HEROS,h)
+		json.dump(JSON_HEROS, h)
 	with open(SAVE_PATH_JSON+"common.json", 'w') as c:
 		# Formatage et mise dans le json
-		json.dump(JSON_CARDS_COMMON,c)
+		json.dump(JSON_CARDS_COMMON, c)
 	with open(SAVE_PATH_JSON+"rare.json", 'w') as r:
 		# Formatage et mise dans le json
-		json.dump(JSON_CARDS_RARE,r)
+		json.dump(JSON_CARDS_RARE, r)
 print("END")
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+from KEY import (API_KEY)
 
 # Lien de API pour les carte
-API_URL = 'https://api.altered.gg/cards'
+API_URL = 'https://api.altered.gg/public/cards'
+
+# "Accept-Language": "fr-FR",
+headers = {"Authorization": API_KEY, "Accept-Language": "fr-FR"}
 
 # Nom et Code du SET [['BTG','CORE'],['TBF','ALIZE']]
-SETALL = [['BTG','CORE'],['TBF','ALIZE']]
+SETALL = [['BTG', 'CORE'], ['TBF', 'ALIZE']]
 
 # Image par page a exporté (36 max)
 ItemPerPage = 36
@@ -12,7 +16,7 @@ ItemPerPage = 36
 PAGE = 16
 
 # Langue exporté
-headers = {"Accept-Language":"fr-FR"} #en-US
+# headers = {"Accept-Language":"fr-FR"} #en-US
 
 # Download PNG
 DOWNLOAD_PNG = False
